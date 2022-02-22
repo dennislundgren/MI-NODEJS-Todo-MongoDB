57b607c3dd26b1b590f9d560a330e1c7f9f11330
Dennis Lundgren <dennis.axel@icloud.com>
2022-02-22 17:39:59 +0100

    outsource credentials, updt status handling

--- database.js
+++ database.js
@@ -2,18 +2,18 @@
 // IMPORTS //
 ////////////
 const { MongoClient } = require("mongodb");
+const { mongoUser, mongoPassword } = require("./credentials");
 ///////////////
 // SETTINGS //
 /////////////
-const CONNECTION_STRING =
-  "mongodb+srv://Debranch:0JaT7K6cSe5HAoLI@cluster0.o9alt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const CONNECTION_STRING = `mongodb+srv://${mongoUser}:${mongoPassword}@cluster0.o9alt.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
 ////////////////
 // FUNCTIONS //
 //////////////
 async function getDb(database) {
   const client = new MongoClient(CONNECTION_STRING);
   await client.connect();
-  const db = await client.db(database);
+  const db = client.db(database);
   return db;
 }
 //////////////
